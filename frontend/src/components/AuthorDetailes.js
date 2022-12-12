f99ee3b78be9e12fdb85999f3709ac9bf3b13976
Foxxy80orange80 <d.emelianoff@gmail.com>
2022-12-12 16:56:00 +0300

    la final'

---
+++ frontend/src/components/AuthorDetailes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorDetailes = () => {
+  return <div className='detailes'>
+    <label className='author name'>Emelianov Dmitriy Sergeevich</label><br/>
+    <label className='author group'>P32111</label><br/>
+    <label className='author variant'>Variant: 1881</label><br/>
+  </div>;
+};
+
+export default AuthorDetailes;
